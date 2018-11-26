1cd5f468bfbbd5f2d8bd8567d70c4ceda64b41de
Antti Niiles <antti.niiles@gmail.com>
2018-11-26 20:33:13 +0200

    fine tuned version

---
+++ react_app/build/precache-manifest.a5abb4b244ec20572074264cbc626bd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65bc6c62d843c1914ab4",
+    "url": "/static/css/main.5ebb0516.chunk.css"
+  },
+  {
+    "revision": "65bc6c62d843c1914ab4",
+    "url": "/static/js/main.65bc6c62.chunk.js"
+  },
+  {
+    "revision": "c94e93b4e79a6493efa5",
+    "url": "/static/js/1.c94e93b4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2f045a7c7d033595afb99bdeff40cdcf",
+    "url": "/index.html"
+  }
+];
